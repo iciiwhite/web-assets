22eea37b8849ae932693bcfbfac4c67bc3ea4a4c
Icii White <donkeyhuman7@gmail.com>
2026-02-03 18:29:30 +0300

    Update file

--- icons/icons.js
+++ icons/icons.js
@@ -117,6 +117,7 @@
 'upload':'<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 15V17C19 18.1046 18.1046 19 17 19H7C5.89543 19 5 18.1046 5 17V15M12 15L12 5M12 5L14 7M12 5L10 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>'
 
 
+
 };})();
 
 
